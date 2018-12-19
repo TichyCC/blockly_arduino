fd7f750bca56b1a22b7b96b7415e8c10ed8adbf5
TichyCC <44693547+TichyCC@users.noreply.github.com>
2018-12-19 21:41:00 +0800

    edit log.js

--- workDir/log.js
+++ workDir/log.js
@@ -11,29 +11,33 @@ const errFunc = (err) => {
 
 //events records
 let movingFunc = (event) => {
+  let d = new Date();
   if(event.type == Blockly.Events.MOVE) {
-    fs.appendFile(path, "Move\n", errFunc);
+    fs.appendFile(path, "Moving, " + d.toString() + "\n", errFunc);
   }
 }
 Code.workspace.addChangeListener(movingFunc);
 
 let creatFunc = (event) => {
+  let d = new Date();
   if(event.type == Blockly.Events.BLOCK_CREATE) {
-    fs.appendFile(path, "creat\n", errFunc);
+    fs.appendFile(path, "creating, " + d.toString() + "\n", errFunc);
   }
 }
 Code.workspace.addChangeListener(creatFunc);
 
 let deleteFunc = (event) => {
+  let d = new Date();
   if(event.type == Blockly.Events.DELETE) {
-    fs.appendFile(path, "delete\n", errFunc);
+    fs.appendFile(path, "deleting, " + d.toString() + "\n", errFunc);
   }
 }
 Code.workspace.addChangeListener(deleteFunc);
 
 let changeFunc = (event) => {
+  let d = new Date();
   if(event.type == Blockly.Events.CHANGE) {
-    fs.appendFile(path, "change\n", errFunc);
+    fs.appendFile(path, "changing, " + d.toString() + "\n", errFunc);
   }
 }
 Code.workspace.addChangeListener(changeFunc);
